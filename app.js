34a325d5dedea8b0a783f1ba4474b3ffc6a86fad
Tony <csd4262@csd.uoc.gr>
2023-01-30 21:31:39 +0200

    created main routes

--- app.js
+++ app.js
@@ -11,5 +11,21 @@ app.set('view engine', 'ejs');
 app.listen(port);
 
 app.get('/', function(req, res) {
-    res.render('index');
+    res.render('index', {title:'homepage'});
+});
+
+app.get('/about', function(req, res) {
+    res.render('about', {title:'About'});
+});
+
+app.get('/log-in', function(req, res) {
+    res.render('log_in', {title:'Log In'});
+});
+
+app.get('/log-out', function(req, res) {
+    res.render('log_out', {title:'Log Out'});
+});
+
+app.get('/sign-up', function(req, res) {
+    res.render('sign_up', {title:'Sign Up'});
 });
