5e0bfd4a2e2d27c3e74fa42214b83eaabf3ad8ed
Tony <csd4262@csd.uoc.gr>
2023-01-30 18:03:38 +0200

    Added some views

--- app.js
+++ app.js
@@ -1,2 +1,15 @@
-console.log('hello world');
-console.log('second commit');
+const express = require('express');
+
+// express app
+const app = express();
+const port = 3000;
+
+// register view engine
+app.set('view engine', 'ejs');
+
+// listen for requests
+app.listen(port);
+
+app.get('/', function(req, res) {
+    res.render('index');
+});
